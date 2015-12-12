47d9072bd661247e0064b468c4df644dcfa41c1a
Eirik Krogstad <eirikkr@gmail.com>
2015-12-13 00:43:39 +0100

    Add watch mode to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,16 @@
 var gulp  = require('gulp'),
     babel = require('gulp-babel')
 
-function log(message) {
-  console.log(message.toString())
-  this.emit('end')
-}
-
 gulp.task('build', function() {
   return gulp.src('src/*.js')
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(gulp.dest('scripts'))
+})
+
+gulp.task('watch', ['build'], function() {
+  gulp.watch(['src/*.js'], ['build'])
 })
 
 gulp.task('default', ['build'])
